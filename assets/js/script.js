e8839927fcec7757870d1c7f4bdb2415e1a1b91d
Prodeveder <72999777+Prodeveder@users.noreply.github.com>
2024-10-14 12:17:10 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const generate = document.querySelector(".generate");
 generate.addEventListener("click", () => {
   let randomIndex = Math.floor(Math.random() * 6);
 
-  fetch("../assets/js/data.json")
+  fetch("https://prodeveder.github.io/VerseVault/assets/js/data.json")
     .then((response) => response.json())
     .then((data) => changeVerse(data));
 });
@@ -13,11 +13,8 @@ const verseText = document.querySelector(".card__text p");
 const verseTopic = document.querySelector(".topic");
 
 const changeVerse = (data) => {
-  // Generate Random Digits
   let randomIndex = Math.floor(Math.random() * 9),
     randomIndexVerse = Math.floor(Math.random() * 5);
-
-  console.log(data.BibleVerses);
 
   verseHeader.textContent =
     data.BibleVerses[randomIndex].verses[randomIndexVerse].reference;
